87bcad77a97d8f1aeda40b91b5f44e5a7205acc1
Kent Johnson <kent@fireshark.com>
2019-10-15 13:02:54 -0500

    Add project files.

---
+++ Butterfly.Util/RandomUtil.cs
@@ -0,0 +1,20 @@
+﻿/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+using System;
+using System.Security.Cryptography;
+
+namespace Butterfly.Util {
+    public static class RandomUtil {
+        static readonly RNGCryptoServiceProvider provider = new RNGCryptoServiceProvider();
+
+        public static int Next(int max) {
+            byte[] rand = new byte[4];
+            provider.GetBytes(rand);
+            int i = BitConverter.ToUInt16(rand, 0);
+            return i % max;
+        }
+
+    }
+}
